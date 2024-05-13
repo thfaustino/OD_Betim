c881431f793856a59985295bf69313671272ea26
Thiago Faustino <thfaustino@gmail.com>
2024-05-13 20:11:35 -0300

    OD finalizada falta calcular dados de IR

--- OD_Betim.R
+++ OD_Betim.R
@@ -1,27 +1,17 @@
+options(scipen = 999)
+
 
 #LIBRARIES
-library(readxl)
 library(plyr)
-library(dplyr)
-library(tidyr)
-library(data.table)
 library(stringr)
-library(sp)
-library(terra)
-library(geosphere)
+library(readxl)
+library(dplyr)
 
 #CARREGANDO ARQUIVOS
-setwd('GPS')
-myfiles = list.files(path=getwd(),pattern="*.xls*",full.names=TRUE)
-GPS = ldply(myfiles, read_excel)
-setwd('..')
+setwd('D:/BETIM/OD/01_DADOS_BRUTOS')
 setwd('SBE')
 myfiles = list.files(path=getwd(),pattern="*.csv",full.names=TRUE)
 SBE = ldply(myfiles, read.csv)
-setwd('..')
-setwd('PED')
-myfiles = list.files(path=getwd(),pattern="*.csv",full.names=TRUE)
-PED = ldply(myfiles, read.csv, sep=";",)
 setwd('..')
 rm(myfiles)
 
@@ -32,7 +22,7 @@ SBE[,1]<-NULL
 for(j in 1:ncol(SBE)){
   if(is.na(SBE[1,j]) & j!=ncol(SBE)){
     SBE[,j]<-SBE[,j+1]
- }
+  }
 }
 rm(j)
 SBE$X.1<-NULL
@@ -48,46 +38,13 @@ SBE$X.10<-NULL
 colnames(SBE)<-c("CARTAO","TP_USUARIO","DATA_UTILIZACAO","HORA_UTILIZACAO","LATITUDE","LONGITUDE","TP_PGTO","COD_LINHA","VEIC_PROG","VEIC_REAL","HORA_ABERTURA","VALOR_COBRADO")
 
 #GPS
-GPS<- GPS %>% dplyr::select(Veículo,`Data da Posição`,`Grade de Operação`,...6,Latitude,Longitude)
-colnames(GPS)<-c("VEIC","DATA_HORA","LINHA","SENTIDO","LATITUDE","LONGITUDE")
-GPS<-filter(GPS,!is.na(GPS$VEIC))
+setwd('GPS')
+myfiles = list.files(path=getwd(),pattern="*.xls*",full.names=TRUE)
+SBE2<-SBE
 
-#PED
-colnames(PED)<-c("PED_COD","DESC","LATITUDE","LONGITUDE","ROUTES")
-PED<-separate(PED,ROUTES, into=paste0("ROUTES",1:100),sep=',')
-PED<-pivot_longer(PED,cols=colnames(PED[,5:ncol(PED)]),values_to = "LINHA")
-PED$name<-NULL             
-PED<-filter(PED,!is.na(PED$LINHA))             
-PED$LINHA<-gsub(' ','',PED$LINHA)
-PED$LINHA<-as.character(PED$LINHA)
-PED<-as.data.frame(PED)
-
-#CRIANDO CHAVES PARA ASSOCIACAO NO EMBARQUE
-GPS$CHAVE<-paste(GPS$VEIC,GPS$DATA_HORA)
-GPS$CHAVE<-gsub(':','',GPS$CHAVE)
-GPS$CHAVE<-gsub(' ','',GPS$CHAVE)
-GPS$CHAVE<-gsub('-','',GPS$CHAVE)
-GPS$CHAVE<-gsub('/','',GPS$CHAVE)
-GPS$CHAVE<-as.numeric(GPS$CHAVE)
-GPS<-arrange(GPS,VEIC,DATA_HORA)
-SBE$CHAVE<-paste(SBE$VEIC_PROG,str_sub(SBE$DATA_UTILIZACAO,start=-4),str_sub(SBE$DATA_UTILIZACAO,start=4,end=5),str_sub(SBE$DATA_UTILIZACAO,end=2),SBE$HORA_UTILIZACAO)
-SBE$CHAVE<-gsub(':','',SBE$CHAVE)
-SBE$CHAVE<-gsub(' ','',SBE$CHAVE)
-SBE$CHAVE<-gsub('-','',SBE$CHAVE)
-SBE$CHAVE<-gsub('/','',SBE$CHAVE)
-SBE$CHAVE<-gsub('A','',SBE$CHAVE)
-SBE$CHAVE<-as.numeric(SBE$CHAVE)
-
-##COMPATIBILIZANDO DIAS DE GPS COM O SBE E RETIRANDO VEÍCULOS COM FALHAS NA COMUNICAÇÃO
-SBE<-filter(SBE,gsub('A','',SBE$VEIC_PROG) %in% GPS$VEIC)
-SBE$DATAHORA_UTILIZACAO<-as.POSIXct(str_c(SBE$DATA_UTILIZACAO,' ',SBE$HORA_UTILIZACAO), format="%d/%m/%Y %H:%M:%S")
-SBE<-filter(SBE,DATAHORA_UTILIZACAO>=min(GPS$DATA_HORA),DATAHORA_UTILIZACAO<=max(GPS$DATA_HORA))
-
-### *** VERIFICAR A NECESSIDADE DE CORRIGIR O NÚMERO DE ORDEM DOS VEÍCULOS DO STPBC**
-# FUNÇÃO PARA ASSOCIAR O CARTAO AO PED
 associar_par_cod_siu <- function(SBE, GPS) {
-  setDT(SBE)
-  setDT(GPS)
+  data.table::setDT(SBE)
+  data.table::setDT(GPS)
   
   # Verificar valores faltantes
   if (any(is.na(SBE$CHAVE))) {
@@ -97,8 +54,8 @@ associar_par_cod_siu <- function(SBE, GPS) {
     stop("Valores faltantes na coluna CHAVE do dataset GPS.")
   }
   
-  setkey(SBE, CHAVE)
-  setkey(GPS, CHAVE)
+  data.table::setkey(SBE, CHAVE)
+  data.table::setkey(GPS, CHAVE)
   indices_proximos <- GPS[SBE, roll = "nearest", on = "CHAVE"]$LINHA
   SBE$LINHA_GPS <- str_sub(indices_proximos,end=4)
   indices_proximos <- GPS[SBE, roll = "nearest", on = "CHAVE"]$SENTIDO
@@ -110,56 +67,157 @@ associar_par_cod_siu <- function(SBE, GPS) {
   return(SBE)
 }
 
-#ASSOCIANDO EMBARQUES
-EMBARQUES <- associar_par_cod_siu(SBE, GPS)
-EMBARQUES$COD_LINHA<-as.character(EMBARQUES$COD_LINHA)
-EMBARQUES$CHAVE<-str_c(EMBARQUES$DATA_UTILIZACAO,EMBARQUES$VEIC_PROG,EMBARQUES$HORA_ABERTURA,sep="-")
-EMBARQUES$LINHA_GPS<-gsub('-','',EMBARQUES$LINHA_GPS)
-EMBARQUES$LINHA_GPS<-gsub(' ','',EMBARQUES$LINHA_GPS)
+for(j in 1:length(myfiles)){
+  
+  SBE<-SBE2
+  GPS = as.data.frame(read_excel(myfiles[j]))
+  GPS<-GPS %>% dplyr::select(Veículo,`Data da Posição`,`Grade de Operação`,...6,Latitude,Longitude)
+  colnames(GPS)<-c("VEIC","DATA_HORA","LINHA","SENTIDO","LATITUDE","LONGITUDE")
+  GPS<-dplyr::filter(GPS,!is.na(GPS$VEIC))
+  GPS$DATA_HORA<-as.character(GPS$DATA_HORA)
+  GPS<-as.data.frame(GPS)
+  
+  #CRIANDO CHAVES PARA ASSOCIACAO NO EMBARQUE
+  GPS$CHAVE<-str_c(GPS$VEIC,GPS$DATA_HORA)
+  GPS$CHAVE<-gsub(':','',GPS$CHAVE)
+  GPS$CHAVE<-gsub(' ','',GPS$CHAVE)
+  GPS$CHAVE<-gsub('-','',GPS$CHAVE)
+  GPS$CHAVE<-gsub('/','',GPS$CHAVE)
+  GPS$CHAVE<-as.numeric(GPS$CHAVE)
+  GPS<-GPS[!duplicated(GPS$CHAVE),]
+  GPS<-arrange(GPS,CHAVE)
+  SBE$DATAHORA_UTILIZACAO<-as.POSIXct(str_c(SBE$DATA_UTILIZACAO,' ',SBE$HORA_UTILIZACAO), format="%d/%m/%Y %H:%M:%S")
+  SBE<-dplyr::filter(SBE,gsub('A','',SBE$VEIC_PROG) %in% GPS$VEIC)
+  SBE$CHAVE<-str_c(SBE$VEIC_PROG,str_sub(SBE$DATA_UTILIZACAO,start=-4),str_sub(SBE$DATA_UTILIZACAO,start=4,end=5),str_sub(SBE$DATA_UTILIZACAO,end=2),SBE$HORA_UTILIZACAO)
+  SBE$CHAVE<-gsub(':','',SBE$CHAVE)
+  SBE$CHAVE<-gsub(' ','',SBE$CHAVE)
+  SBE$CHAVE<-gsub('-','',SBE$CHAVE)
+  SBE$CHAVE<-gsub('/','',SBE$CHAVE)
+  SBE$CHAVE<-gsub('A','',SBE$CHAVE)
+  SBE$CHAVE<-as.numeric(SBE$CHAVE)
+  SBE<-dplyr::filter(SBE,!is.na(CHAVE))
+  
+  ##COMPATIBILIZANDO DIAS DE GPS COM O SBE E RETIRANDO VEÍCULOS COM FALHAS NA COMUNICAÇÃO
+  
+  SBE<-dplyr::filter(SBE,DATAHORA_UTILIZACAO>=min(GPS$DATA_HORA),DATAHORA_UTILIZACAO<=max(GPS$DATA_HORA))
 
-#ASSOCIANDO EMBARQUES COM PEDS
-PED<-filter(PED,LINHA %in% EMBARQUES$LINHA_GPS,LINHA!="")
-
-procura_ped_proximo<-function(x,y,z,w){
-  return(which.min(sqrt(((z-x)^2)+((w-y)^2))))
-}
-
-Cod_PED<-0
-PED2<-PED
-
-for(j in 1:nrow(EMBARQUES)){
-  PED2<-filter(PED,str_sub(LINHA,end=nchar(EMBARQUES$COD_LINHA[j]))==EMBARQUES$COD_LINHA[j])
-  Cod_PED[j]<-procura_ped_proximo(EMBARQUES$LONGITUDE[j],EMBARQUES$LATITUDE[j],PED$LONGITUDE,PED$LATITUDE)
-}
-
-EMBARQUES$COD_PED<-PED$PED_COD[Cod_PED]
-EMBARQUES$LONGITUDE<-PED$LONGITUDE[Cod_PED]
-EMBARQUES$LATITUDE<-PED$LATITUDE[Cod_PED]
-
-#CORRIGINDO LINHA GPS
-
-for(j in 1:nrow(EMBARQUES)){
-  if(EMBARQUES$LINHA_GPS[j]=="" && EMBARQUES$CHAVE[j]==EMBARQUES$CHAVE[j-1]){
-    EMBARQUES$LINHA_GPS[j]==EMBARQUES$LINHA_GPS[j-1]
+  #ASSOCIANDO EMBARQUES
+  EMBARQUES2 <- associar_par_cod_siu(SBE, GPS)
+  if(j==1){
+    EMBARQUES<-EMBARQUES2
+  }else{
+    EMBARQUES<-rbind(EMBARQUES,EMBARQUES2)
   }
+  print(str_c(j," de ",length(myfiles)," finalizado."))
 }
 
-#EMBARQUE_POR_PED_LINHA - SIU HORARIO DA VIAGEM
-EMBARQUES$FX<-as.numeric(str_sub(EMBARQUES$HORA_ABERTURA,end=2))
-EMBARQUES$EXPANSAO<-1
-pivot<-EMBARQUES %>% dplyr::select(COD_PED,COD_LINHA,FX,EXPANSAO) %>% group_by(COD_PED,COD_LINHA,FX) %>% summarise(Passageiros=sum(EXPANSAO))
-pivot$FX<-as.numeric(pivot$FX)
-pivot<-data.frame(pivot)
-h_24<-data.frame(cbind(rep(0,24),rep(0,24),c(0:23),rep(0,24)))
-colnames(h_24)<-c("COD_PED","COD_LINHA","FX","Passageiros")
-pivot<-rbind(h_24,pivot)
-colnames(pivot)<-c("COD_PED","COD_LINHA","FX","Passageiros")
-EMBARQUE_POR_PED<-spread(pivot,FX,Passageiros)
-EMBARQUE_POR_PED[is.na(EMBARQUE_POR_PED)]<-0
-EMBARQUE_POR_PED$"Total Geral"<-EMBARQUE_POR_PED$"0"+EMBARQUE_POR_PED$"1"+EMBARQUE_POR_PED$"2"+EMBARQUE_POR_PED$"3"+EMBARQUE_POR_PED$"4"+EMBARQUE_POR_PED$"5"+EMBARQUE_POR_PED$"6"+EMBARQUE_POR_PED$"7"+EMBARQUE_POR_PED$"8"+EMBARQUE_POR_PED$"9"+EMBARQUE_POR_PED$"10"+EMBARQUE_POR_PED$"11"+EMBARQUE_POR_PED$"12"+EMBARQUE_POR_PED$"13"+EMBARQUE_POR_PED$"14"+EMBARQUE_POR_PED$"15"+EMBARQUE_POR_PED$"16"+EMBARQUE_POR_PED$"17"+EMBARQUE_POR_PED$"18"+EMBARQUE_POR_PED$"19"+EMBARQUE_POR_PED$"20"+EMBARQUE_POR_PED$"21"+EMBARQUE_POR_PED$"22"+EMBARQUE_POR_PED$"23"
-PED2<-PED %>% dplyr::select(PED_COD,DESC,LATITUDE,LONGITUDE)
-PED2<-unique(PED2)
-EMBARQUE_POR_PED<-merge(EMBARQUE_POR_PED,PED2,by.x="COD_PED",by.y="PED_COD")
-EMBARQUE_POR_PED<-EMBARQUE_POR_PED %>% dplyr::select(COD_PED,COD_LINHA,DESC,"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23",`Total Geral`,LATITUDE,LONGITUDE)
-EMBARQUE_POR_PED<-unique(EMBARQUE_POR_PED)
-write.csv(EMBARQUE_POR_PED,"EMBARQUE_POR_PED_HrAbertura.csv",row.names = F)
+#SALVANDO EMBARQUES
+setwd('..')
+setwd('..')
+setwd('02_RESULTADOS')
+write.csv2(EMBARQUES,'EMBARQUES.csv',row.names = F)
+setwd('..')
+rm(j,EMBARQUES2,GPS,SBE,SBE2,myfiles,associar_par_cod_siu)
+
+### TRABALHANDO COM DADOS DE PESQUISA SOBE E DESCE COM SENHA
+#GRADE HEXAGONAL
+setwd('01_DADOS_BRUTOS/LIMITE')
+LIM<-sf::st_read('BETIM.shp')
+extensao<-sf::st_bbox(LIM)
+grid<-sf::st_make_grid(extensao,cellsize = .005, square=F,crs=4326)
+grid<-sf::st_as_sf(grid,crs=4326)
+grid$ID_GRID<-1:nrow(grid)
+
+#CARTAO
+EMBARQUES$ID_val<-c(1:nrow(EMBARQUES))
+VAL<-EMBARQUES %>% dplyr::select(ID_val,LATITUDE,LONGITUDE)
+VAL<-sf::st_as_sf(VAL,coords=c("LONGITUDE","LATITUDE"),crs=4326)
+VAL<-sf::st_join(VAL,grid)
+VAL<-as.data.frame(VAL)
+EMBARQUES$ID_grid<-VAL$ID_GRID
+rm(VAL,LIM,extensao)
+
+#RETIRANDO VALE SOCIAL E DINHEIRO (CARTÕES COM DESEMBARQUE NÃO RASTREÁVEIS)
+OD<-EMBARQUES %>% dplyr::select(CARTAO,TP_USUARIO,DATA_UTILIZACAO,HORA_UTILIZACAO,COD_LINHA,HORA_ABERTURA,SENTIDO,ID_grid)
+OD<-arrange(OD,CARTAO,DATA_UTILIZACAO,HORA_UTILIZACAO)
+DIN<-rbind(dplyr::filter(OD,CARTAO=='0'),dplyr::filter(OD,TP_USUARIO=='VALE SOC'))
+OD<-dplyr::filter(OD,CARTAO!=0)
+OD<-dplyr::filter(OD,TP_USUARIO!='VALE SOC')
+
+#PRIMEIRA VALIDAÇÃO DO DIA
+OD$CARTAO_DIA<-str_c(OD$CARTAO,OD$DATA_UTILIZACAO)
+val1<-OD %>% dplyr::select(CARTAO_DIA,ID_grid)
+val1<-val1[!duplicated(val1$CARTAO_DIA),]
+colnames(val1)<-c("CARTAO_DIA","VAL1")
+OD<-merge(OD,val1,all.x=T)
+rm(val1)
+OD<-arrange(OD,CARTAO,DATA_UTILIZACAO,HORA_UTILIZACAO)
+
+#SEGUNDA VALIDAÇÃO: PROXIMAS VALIDAÇÕES COM INTERVALO MENOR DO QUE 5 MINUTOS E NO MESMO DIA
+OD$ID_DESTINO <- OD$VAL1
+indices <- (OD$CARTAO_DIA[-1] == OD$CARTAO_DIA[-nrow(OD)]) & (as.POSIXct(OD$HORA_UTILIZACAO[-1], format = "%H:%M")-as.POSIXct(OD$HORA_UTILIZACAO[-nrow(OD)], format = "%H:%M")<300)
+indices <- which(indices)
+DIN<-rbind(DIN,OD[indices+1,] %>% dplyr::select(colnames(DIN)))
+indices<-indices+1
+OD<-OD[-indices,]
+indices <- (OD$CARTAO_DIA[-1] == OD$CARTAO_DIA[-nrow(OD)]) & (as.POSIXct(OD$HORA_UTILIZACAO[-1], format = "%H:%M")-as.POSIXct(OD$HORA_UTILIZACAO[-nrow(OD)], format = "%H:%M")>=300)
+indices <- which(indices)
+OD$ID_DESTINO[indices]<-OD$ID_grid[indices+1]
+rm(i,indices)
+
+#VALIDACOES NA MESMA GRADE
+DIN<-rbind(DIN,dplyr::filter(OD,ID_DESTINO==ID_grid) %>% dplyr::select(colnames(DIN)))
+OD<-filter(OD,ID_DESTINO!=ID_grid)
+OD$VAL1<-NULL
+OD$CARTAO_DIA<-NULL
+
+#EXPANSÃO: DESTINO PARA CARTÕES SEM PAR O-D
+DIN$CHAVE<-str_c(DIN$COD_LINHA,DIN$ID_grid,str_sub(DIN$HORA_UTILIZACAO,end=2),sep='-')
+DIN$COUNTER<-1
+OD$CHAVE<-str_c(OD$COD_LINHA,OD$ID_grid,str_sub(OD$HORA_UTILIZACAO,end=2),sep="-")
+OD$COUNTER<-1
+ODfe<-OD %>% dplyr::select(CHAVE,COUNTER) %>% dplyr::group_by(CHAVE) %>% dplyr::summarise(QTDod=sum(COUNTER))
+DINfe<-DIN %>% dplyr::select(CHAVE,COUNTER) %>% dplyr::group_by(CHAVE) %>% dplyr::summarise(QTDo=sum(COUNTER))
+FE1<-merge(ODfe,DINfe,all.x=T,all.y=T)
+FE1[is.na(FE1)]<-0
+FE1$FE1<-(FE1$QTDod+FE1$QTDo)/(FE1$QTDod)
+OD<-merge(OD,FE1 %>% dplyr::select(CHAVE,FE1),all.x=T)
+rm(ODfe,DINfe,FE1)
+
+#EXPANSÃO: CARTÕES SEM PAR O/D E SEM CARTOES COM PAR OD NO HEXÁGONO NA VIAGEM
+OD$CHAVE<-stringr::str_c(OD$COD_LINHA,OD$DATA_UTILIZACAO,OD$HORA_ABERTURA,sep="-")
+DIN$CHAVE<-stringr::str_c(DIN$COD_LINHA,DIN$DATA_UTILIZACAO,DIN$HORA_ABERTURA,sep="-")
+ODfe<-OD %>% dplyr::select(CHAVE,FE1) %>% dplyr::group_by(CHAVE) %>% dplyr::summarise(QTDod=sum(FE1))
+DINfe<-DIN %>% dplyr::select(CHAVE,COUNTER) %>% dplyr::group_by(CHAVE) %>% dplyr::summarise(QTDo=sum(COUNTER))
+FE2<-merge(ODfe,DINfe,all.x=T,all.y=T)
+FE2$QTDo[is.na(FE2$QTDo)]<-0
+FE2$FE2<-(FE2$QTDod+FE2$QTDo)/(FE2$QTDod)
+OD<-merge(OD,FE2 %>% dplyr::select(CHAVE,FE2),all.x=T)
+
+#EXPANSÃO: CARTÕES SEM PAR O/D E SEM CARTOES COM PAR OD NO HEXÁGONO NA LINHA
+OD$CHAVE<-stringr::str_c(OD$COD_LINHA,OD$DATA_UTILIZACAO,sep="-")
+EMBARQUES$COUNTER<-1
+EMBARQUES$CHAVE<-stringr::str_c(EMBARQUES$COD_LINHA,EMBARQUES$DATA_UTILIZACAO,sep="-")
+ODfe<-OD %>% dplyr::select(CHAVE,FE2) %>% dplyr::group_by(CHAVE) %>% dplyr::summarise(QTDod=sum(FE2))
+DINfe<-EMBARQUES %>% dplyr::select(CHAVE,COUNTER) %>% dplyr::group_by(CHAVE) %>% dplyr::summarise(QTDo=sum(COUNTER))
+FE4<-merge(ODfe,DINfe,all.x=T,all.y=T)
+FE4$QTDo[is.na(FE4$QTDo)]<-0
+FE4$FE4<-(FE4$QTDo)/(FE4$QTDod)
+OD<-merge(OD,FE4 %>% dplyr::select(CHAVE,FE4),all.x=T)
+OD$FE<-OD$FE4*OD$FE2
+rm(EMBARQUES,DIN,DINfe,FE2,FE3,FE4,ODfe)
+OD$CHAVE<-NULL
+OD<-arrange(OD,COD_LINHA,DATA_UTILIZACAO,HORA_ABERTURA,HORA_UTILIZACAO)
+OD$FE1<-NULL
+OD$FE2<-NULL
+OD$COUNTER<-NULL
+OD$FE4<-NULL
+
+#SALVANDO RESULTADOS
+setwd('..')
+setwd('..')
+setwd('02_RESULTADOS')
+write.csv2(OD,"SDCS.csv",row.names = F)
+
+#CALCULANDO PTC POR VIAGEM
+sf::st_write(grid,'grid.json',driver="GeoJSON",crs=4326)
